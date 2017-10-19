649bbbb97b58a6a9f5151b921c6c31fda3ec77db
Klebersonreis <klebersonlr@gmail.com>
2017-10-19 17:13:24 -0200

    Add files via upload
    Novos arquivos.

---
+++ foo004.1.py
@@ -0,0 +1,28 @@
+import unittest
+
+class Area:
+    def quadrada(self):
+        return self.lado1 * self.lado2
+
+    def cubica(self):
+        return self.lado1 * self.lado2 * self.lado3
+        
+class MyCalcTest(unittest.TestCase):
+    def testAreaQuadrada(self):
+        area = Area()
+        area.lado1 = 3
+        area.lado2 = 9
+
+        self.assertEqual(27, area.quadrada())
+
+    def testAreaCubica(self):
+        area = Area()
+        area.lado1 = 3
+        area.lado2 = 9 
+        area.lado3 = 2
+        
+        self.assertEqual(54, area.cubica())
+
+
+if __name__ == '__main__':
+    unittest.main()
