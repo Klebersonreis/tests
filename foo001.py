1f0abea975422e84b0ede2c6624d82fc426701a5
Klebersonreis <klebersonlr@gmail.com>
2017-10-20 18:03:21 -0200

    Add files via upload
    Cópia dos arquivos.

---
+++ foo001.py
@@ -0,0 +1,8 @@
+num = 5
+#funcao dobro de num que e 5 resultado 10
+def dobro(num):
+    return num * 2
+    pass
+
+assert 10 == dobro(num)
+#assert declaracao (statement) usado em validacoes exemplo assertequal
---
+++ foo003.py
@@ -0,0 +1,20 @@
+import unittest
+
+#definicao da classe Juros
+class Juros:
+    #definicao da funcao simples 
+    def simples(self):
+        return self.capital * self.taxa * self.n_periodos
+
+
+class MyCalcTest(unittest.TestCase):
+    def testSimples(self):
+        calcJuros = Juros()
+        calcJuros.capital      = 16000;
+        calcJuros.taxa         = 0.04;
+        calcJuros.n_periodos   = 4;
+
+        self.assertEqual(2560, calcJuros.simples())
+
+if __name__ == '__main__':
+    unittest.main()
